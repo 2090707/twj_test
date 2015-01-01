b8ebc797f97b92dd82062558486edc27c0fdc33d
Chinook- <rammluft@gmail.com>
2015-01-01 00:15:08 +0000

    restricted page etc

--- rango/views.py
+++ rango/views.py
@@ -22,7 +22,7 @@ def about(request):
 	
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html', {})
 
 def category(request, category_name_slug):
     context_dict = {}
